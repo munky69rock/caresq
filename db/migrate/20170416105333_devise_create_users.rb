83a7766eccb77faf09696a4e8459be3eb293e404
Masayuki Uehara <uehara.masayuki@dena.jp>
2017-04-16 22:09:56 +0900

    Added name property to user

--- db/migrate/20170416105333_devise_create_users.rb
+++ db/migrate/20170416105333_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :name, null: false
       t.date :birth_date
       t.integer :gender
       t.text :description
