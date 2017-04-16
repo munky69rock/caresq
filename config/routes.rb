557d7f4e7653f5c39921fd2ed499be3ada0c76c7
Masayuki Uehara <uehara.masayuki@dena.jp>
2017-04-16 22:01:07 +0900

    Change default devise config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: [:sessions, :registrations]
+  devise_scope :user do
+    get :login, to: 'devise/sessions#new', as: :new_user_session
+    post :login, to: 'devise/sessions#create', as: :user_session
+    delete :logout, to: 'devise/sessions#destroy', as: :destroy_user_session
+
+    get :signup, to: 'devise/registrations#new', as: :new_user_registration
+    scope :users do
+      get    :cancel, to: 'devise/registrations#cancel',  as: :cancel_user_registration
+      get    :edit,   to: 'devise/registrations#edit',    as: :edit_user_registration
+      patch  :/,      to: 'devise/registrations#update',  as: :user_registration
+      put    :/,      to: 'devise/registrations#update'
+      post   :/,      to: 'devise/registrations#create'
+    end
+  end
+
+  root 'roots#index'
 end
