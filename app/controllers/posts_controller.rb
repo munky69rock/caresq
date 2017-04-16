a94051372b6857455d06f83bde45c8fc73026b8c
Masayuki Uehara <uehara.masayuki@dena.jp>
2017-04-17 00:35:22 +0900

    Enabled to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,17 +4,35 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_and_belongs_to_many :tags
+
+  validates :title, presence: true
+  validates :body, presence: true
 end
