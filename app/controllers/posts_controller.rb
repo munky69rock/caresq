d72297edcbdddb76fb08e4b840a8478e3506f90e
Masayuki Uehara <uehara.masayuki@dena.jp>
2017-04-26 00:47:05 +0900

    Adjusted layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.includes(comments: [:user]).find(params[:id])
     @comment = Comment.new post: @post, user: current_user
   end
 
