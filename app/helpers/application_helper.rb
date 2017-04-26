a1b2c3e699b15e6dfcf61f81359c17a4e2f2b557
Masayuki Uehara <uehara.masayuki@dena.jp>
2017-04-26 12:16:36 +0900

    Added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
     }
     link_to(name, options, html_options, &block)
   end
+
+  def title(text, size = 2)
+    content_for :title, text
+    "<h#{size}>#{text}</h#{size}>".html_safe
+  end
 end
