2296b4d98b2365966f2774d31b59fa390755b0cf
Masayuki Uehara <uehara.masayuki@dena.jp>
2017-05-06 17:24:00 +0900

    Limited access to herokuapp.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :verify_domain
 
   private
+
+  def verify_domain
+    domain = request.domain
+    return if Settings.domain.nil? || Settings.domain == domain
+    url = URI.parse(request.original_url)
+    url.domain = Settings.domain
+    redirect_to url.to_s, status: 301
+  end
 
   def after_sign_in_path_for(_resource)
     blacklist = [new_user_session_path, new_user_registration_path]
