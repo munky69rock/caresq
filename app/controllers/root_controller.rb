6611bf5fcc0778208bf7321db884c86a035f9eb4
Masayuki Uehara <uehara.masayuki@dena.jp>
2017-06-22 02:36:22 +0900

    Fixed nil error

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,6 +4,10 @@ class RootController < ApplicationController
   def index
     @post = Post.new if current_user.try(:confirmed?)
     @posts = Post.includes(:user, :tags).order(id: :desc).page params[:page]
-    @likes = current_user.likes.where(post_id: @posts.map(&:id)).pluck(:post_id)
+    @likes = if current_user.try(:confirmed?)
+               current_user.likes.where(post_id: @posts.map(&:id)).pluck(:post_id)
+             else
+               []
+             end
   end
 end
